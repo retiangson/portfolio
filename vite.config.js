61dc30449d322602622cf64a23a49fe23681a04c
retiangson <116651559+retiangson@users.noreply.github.com>
2025-09-21 12:28:14 +1200

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace `your-repo-name` with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/",
+  base: "/portfolio/",
 })
