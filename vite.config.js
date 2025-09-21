060fe5ec13f9852f160ff731e604c7978a8cb03a
retiangson <116651559+retiangson@users.noreply.github.com>
2025-09-21 17:12:23 +1200

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/",   // 👈 must match your repo name exactly
+  base: "/portfolio/",   // 👈 must match your repo name exactly
 });
